2f9eea229020fa04bdcb9decb51387fa2e1b4af9
ALEXANDER TORRES HERNANDEZ <114458694+alexkun90@users.noreply.github.com>
2023-11-14 12:17:18 -0600

    DeNuevo
    Subiendo lo que teniamos

---
+++ iLib-main/ProyectoBiblioteca.sql
@@ -0,0 +1,62 @@
+-- Eliminamos las configuraciones específicas de MySQL
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- SET AUTOCOMMIT = 0;
+-- START TRANSACTION;
+-- SET time_zone = "+00:00";
+-- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- /*!40101 SET NAMES utf8mb4 */;
+
+-- Crear la secuencia para el campo autoincremental
+CREATE SEQUENCE books_seq;
+
+-- Estructura de tabla para la tabla `books`
+CREATE TABLE books (
+  id NUMBER PRIMARY KEY,
+  title VARCHAR2(255) NOT NULL,
+  publication_date VARCHAR2(255) NOT NULL,
+  author VARCHAR2(255) NOT NULL,
+  category VARCHAR2(255) NOT NULL,
+  "edit" VARCHAR2(255) NOT NULL,
+  lang VARCHAR2(255) NOT NULL,
+  pages VARCHAR2(255) NOT NULL,
+  description VARCHAR2(255) NOT NULL,
+  ejemplares VARCHAR2(255) NOT NULL,
+  stock NUMBER NOT NULL,
+  available NUMBER NOT NULL
+);
+
+-- Crear la secuencia para el campo autoincremental
+CREATE SEQUENCE lendings_seq;
+
+-- Estructura de tabla para la tabla `lendings`
+CREATE TABLE lendings_table (
+  id NUMBER PRIMARY KEY,
+  user_id NUMBER NOT NULL,
+  book_id NUMBER NOT NULL,
+  date_out VARCHAR2(255) NOT NULL,
+  date_return VARCHAR2(255)
+);
+
+-- Crear la secuencia para el campo autoincremental
+CREATE SEQUENCE users_seq;
+
+-- Estructura de tabla para la tabla `users`
+CREATE TABLE users (
+  id NUMBER PRIMARY KEY NOT NULL,
+  name VARCHAR2(30) NOT NULL,
+  last_name_p VARCHAR2(30) NOT NULL,
+  last_name_m VARCHAR2(30) NOT NULL,
+  domicilio VARCHAR2(250),
+  tel VARCHAR2(25),
+  sanctions NUMBER DEFAULT 0,
+  sanc_money NUMBER DEFAULT 0 NOT NULL
+);
+
+-- Crear las restricciones de clave foránea
+ALTER TABLE lendings_table ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE lendings_table ADD CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id);
+
+INSERT INTO USERS (ID, NAME, LAST_NAME_P, LAST_NAME_M, DOMICILIO, TEL)
+VALUES (users_seq.NEXTVAL, 'Oscar', 'Doe', 'Smith', '133 Main St', '533-1234');
---
+++ iLib-main/src/main/java/com/mycompany/db/Database.java
@@ -0,0 +1,39 @@
+package com.mycompany.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Database {
+    
+    protected Connection conexion;
+    private final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+    
+    private final String USER = "HR";
+    private final String PASS = "12345";
+    
+     public void Conectar() throws ClassNotFoundException, SQLException {
+        try {
+            // Primero, cargar el controlador
+            Class.forName(JDBC_DRIVER);
+            
+            // Luego, establecer la conexión
+            conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException ex) {
+            // Cambiar el tipo de excepción lanzada
+            throw new SQLException("Error al conectar a la base de datos Oracle", ex);
+        }
+    }
+
+    public void Cerrar() throws SQLException {
+        if (conexion != null) {
+            if (!conexion.isClosed()) {
+                conexion.close();
+            }
+        }
+    }
+}
