13692d413637ad1343151d18fc2d20dcbf95c905
DeSotoCR <aleo242512@gmail.com>
2023-11-19 18:48:04 -0600

    DAOMultasImpl
    Implementacion completa de los metodos

--- iLib-main/src/main/java/com/mycompany/ilib/DAOMultasImpl.java
+++ iLib-main/src/main/java/com/mycompany/ilib/DAOMultasImpl.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class DAOMultasImpl extends Database implements DAOMultas{
     
     public void registrar(Multas multa) throws Exception {
@@ -26,6 +27,84 @@ public class DAOMultasImpl extends Database implements DAOMultas{
         } finally {
             this.Cerrar();
         }
+    }
+    
+    public void modificar(Multas multa) throws Exception {
+        try {
+            this.Conectar();
+            PreparedStatement st = this.conexion.prepareStatement("UPDATE multas SET id_multa = ?, id_users = ?, monto = ?, fecha_vencimiento = ?, \"estado_multa\" = ?  WHERE id_multa = ?");
+            st.setInt(1, multa.getId_multa());
+            st.setInt(2, multa.getId_users());
+            st.setFloat(3, multa.getMonto());
+            st.setString(4, multa.getFecha_vencimiento());
+            st.setString(5, multa.getEstado_multa());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+    
+    public void eliminar(int multaid) throws Exception {
+        try {
+            this.Conectar();
+            PreparedStatement st = this.conexion.prepareStatement("DELETE FROM books WHERE id_multa = ?");
+            st.setInt(1, multaid);
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+    
+    public List<Multas> listar() throws Exception {
+        List<Multas> lista = null;
+        try {
+            this.Conectar();
+            PreparedStatement st = this.conexion.prepareStatement("SELECT * FROM multas_table ORDER BY id DESC");
+
+            lista = new ArrayList<>();
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Multas multa = new Multas();
+                multa.setId_multa(rs.getInt("id"));
+                multa.setId_users(rs.getInt("user_id"));
+                multa.setFecha_vencimiento(rs.getString("fecha_vencimiento"));
+                multa.setEstado_multa(rs.getString("estado_multa"));
+                multa.setMonto(rs.getFloat("monto"));
+                lista.add(multa);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return lista;
+    }
+    
+    public Multas getMultaById(int multaId) throws Exception {
+        Multas multa = null;
+        try {
+            this.Conectar();
+            PreparedStatement st = this.conexion.prepareStatement("SELECT * FROM multas WHERE id = ? FETCH FIRST 1 ROWS ONLY");
+            st.setInt(1, multaId);
+
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                multa = new Multas();
+                multa.setId_multa(rs.getInt("id"));
+                multa.setId_users(rs.getInt("user_id"));
+                multa.setFecha_vencimiento(rs.getString("fecha_vencimiento"));
+                multa.setEstado_multa(rs.getString("estado_multa"));
+                multa.setMonto(rs.getFloat("monto"));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return multa;
     }
     
 }
--- iLib-main/src/main/java/com/mycompany/interfaces/DAOMultas.java
+++ iLib-main/src/main/java/com/mycompany/interfaces/DAOMultas.java
@@ -1,7 +1,13 @@
 
 package com.mycompany.interfaces;
+import com.mycompany.models.Multas;
+import java.util.List;
 
 
 public interface DAOMultas {
-    
+    public void registrar(Multas multa) throws Exception;
+    public void modificar(Multas multa) throws Exception;
+    public void eliminar(int multaid) throws Exception;
+    public List<Multas> listar() throws Exception;
+    public Multas getMultaById(int userId) throws Exception;
 }
